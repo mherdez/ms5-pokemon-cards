b366ef7f8b01e72f2619e106706420bc2e36ef1d
Herdez <mherdez@users.noreply.github.com>
2022-11-08 12:25:24 -0600

    tarjetas pokemon

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const container = document.querySelector('.container');
+
+function viewPokemon( pokemon ) {
+  container.innerHTML += `<div class="card ${pokemon.types[0].type.name}">
+  <img src="${pokemon.sprites.other.dream_world.front_default}" alt="">
+  <p>${pokemon.name}</p>
+  <p>${pokemon.types[0].type.name}</p>
+  <div class="info">
+    ${ pokemon.stats.map( item =>
+      `<div class="stats">
+        <div class="stat">${item.stat.name}:</div><div class="stat-valor">${item.base_stat}</div>
+      </div>`
+    ).join(' ')
+  }
+  </div>
+  </div>`
+}
+
+function getPokemon( id ) {
+  const resp = fetch("https://pokeapi.co/api/v2/pokemon/" + id);
+  resp.then( data => {
+  data.json().then( pokemon => viewPokemon( pokemon ))
+  })
+}
+
+for (let index = 0; index < 100; index++) {
+  getPokemon(index)
+
+}
