b25111826ed947a060478c32a111efe3e698f74b
Herdez <mherdez@users.noreply.github.com>
2022-11-09 13:01:22 -0600

    NUEVA VERSION SIN TERMINAR

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
 const container = document.querySelector('.container');
 
+
 function viewPokemon( pokemon ) {
   container.innerHTML += `<div class="card ${pokemon.types[0].type.name}">
   <img src="${pokemon.sprites.other.dream_world.front_default}" alt="">
-  <p>${pokemon.name}</p>
+  <p>${pokemon.types[0].type.name} - ${pokemon.name}</p>
   <div class="info">
     ${ pokemon.stats.map( item =>
       `<div class="stats">
@@ -16,14 +17,46 @@ function viewPokemon( pokemon ) {
   </div>`
 }
 
+// function viewPokemon( pokemon ) {
+
+//   const div = document.createElement('div')
+//   div.classList.add('card');
+//   div.classList.add(pokemon.types[0].type.name);
+
+//   const img = document.createElement('img')
+//   img.src = pokemon.sprites.other.dream_world.front_default;
+
+
+
+//   container.innerHTML += `<div class="card ${pokemon.types[0].type.name}">
+//   <img src="${pokemon.sprites.other.dream_world.front_default}" alt="">
+//   <p>${pokemon.id} - ${pokemon.name}</p>
+//   <div class="info">
+//     ${ pokemon.stats.map( item =>
+//       `<div class="stats">
+//         <div class="stat">${item.stat.name}:</div><div class="stat-valor">${item.base_stat}</div>
+//       </div>`
+//     ).join(' ')
+//   }
+//   </div>
+//   </div>`
+// }
+
 function getPokemon( id ) {
   const resp = fetch("https://pokeapi.co/api/v2/pokemon/" + id);
   resp.then( data => {
-  data.json().then( pokemon => viewPokemon( pokemon ))
+    data.json().then( pokemon => viewPokemon( pokemon ))
   })
 }
 
-for (let index = 1; index <= 16; index++) {
-  getPokemon(index)
-
+const getPokemonAsync = async ( id ) => {
+  const resp = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
+  const data = await resp.json();
+  console.log(data)
+  viewPokemon(data)
 }
+
+for (let index = 1; index <= 40; index++) {
+  getPokemonAsync(index)
+}
+
